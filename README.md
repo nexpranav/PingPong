f97b2897e0cf4dbb573f455b99c0f8faec4067c5
Pranav Dimri <dimripranav03@gmail.com>
2023-09-13 20:56:23 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# PingPongpy
+Python Ping Pong game made using pygame,sys and random
+
+Something I made for a school project.
+
---
+++ finalpong.py
@@ -0,0 +1,137 @@
+import pygame
+import sys
+import random
+
+#General setup
+pygame.init()
+clock = pygame.time.Clock()
+
+def ball_restart():
+    global ball_speed_x,ball_speed_y
+    ball.center = (screen_width/2,screen_length/2)
+    ball_speed_y *= random.choice((1,-1))
+    ball_speed_x *= random.choice((1,-1))
+
+
+def player_animation():
+        if player.top <= 0:
+            player.top = 0
+        if player.bottom >= screen_length:
+            player.bottom = screen_length
+
+def ball_animation():
+    global ball_speed_x,ball_speed_y, player_score, opponent_score
+    
+    if ball.top <= 0 or ball.bottom >= screen_length:
+        ball_speed_y *= -1
+    if ball.left <= 0:
+        ball_restart()
+        player_score += 1
+    if ball.right >= screen_width:
+        ball_restart()
+        opponent_score += 1
+    
+    if ball.left <= 0:
+        ball_restart()
+        player_score += 1
+    
+    if ball.right >= screen_width:
+        ball_restart()
+        opponent_score += 1
+
+
+    if ball.colliderect(player) or ball.colliderect(opponent):
+        ball_speed_x *= -1
+    ball.x += ball_speed_x
+    ball.y += ball_speed_y
+
+def opponent_animation():
+    
+    if opponent.top < ball.y:
+        opponent.top += opponent_speed
+    if opponent.bottom > ball.y:
+        opponent.bottom -= opponent_speed
+    if opponent.top <= 0:
+        opponent.top = 0
+    if opponent.bottom >= screen_length:
+        opponent.bottom = screen_length
+
+#Colors
+bg_color = pygame.Color('grey12')
+light_grey = (200,200,200)
+white = (255,255,255)
+
+#Screen Dimensions
+screen_width = 1280
+screen_length = 700
+screen = pygame.display.set_mode((screen_width,screen_length))
+pygame.display.set_caption('Pong')
+
+#Game objects
+ball = pygame.Rect(screen_width/2 - 15, screen_length/2 - 15,30,30)
+player = pygame.Rect(1260, screen_length/2 - 70,10,140)
+opponent = pygame.Rect(10, screen_width/2 - 70,10,140)
+
+#Animations
+
+player_speed = 0
+ball_speed_x = 7
+ball_speed_y = 7
+opponent_speed = 7
+
+#Text Variables
+player_score = 0
+opponent_score = 0
+game_font = pygame.font.Font("freesansbold.ttf",32)
+
+
+
+#Loop to maintain the window
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                player_speed += 7
+            if event.key == pygame.K_UP:
+                player_speed -= 7
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_DOWN:
+                player_speed -= 7
+            if event.key == pygame.K_UP:
+                player_speed += 7
+    
+    
+    player_animation()
+    ball_animation()   
+    opponent_animation()
+    ball_restart
+    
+    
+    player.y += player_speed
+    
+    #Visuals
+    screen.fill(bg_color)
+    
+    pygame.draw.rect(screen, light_grey, player)
+    pygame.draw.rect(screen, light_grey, opponent)
+    pygame.draw.ellipse(screen, white, ball)
+    pygame.draw.aaline(screen , light_grey, (screen_width/2,0), (screen_width/2,screen_length))
+    
+    player_text = game_font.render(f'{player_score}',False, light_grey)
+    screen.blit(player_text,(660,20))
+
+    opponent_text = game_font.render(f'{opponent_score}',False, light_grey)
+    screen.blit(opponent_text,(600,20))
+
+
+    #Updating window
+    pygame.display.flip()
+    clock.tick(60)
+
+
+
